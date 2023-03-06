c45498beeefbabdae7a6a7a471b81f050fab96a4
tanjwei98 <tanjwei98@gmail.com>
2023-03-06 12:54:29 +0800

    initial template for MLOps

---
+++ Project/core/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path, re_path
+from django.views.generic import RedirectView
+
+from . import views
+
+urlpatterns = [
+    path('main/', views.index, name='main'),
+    path('form/', views.form, name='form'),
+    path('card/', views.card, name='card'),
+    path('chart/', views.chart, name='chart'),
+    path('button/', views.button, name='button'),
+    path('modal/', views.modal, name='modal'),
+    path('table/', views.table, name='table')
+]
---
+++ Project/core/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def form(request):
+    return render(request, "forms.html")
+
+def card(request):
+    return render(request, "cards.html")
+
+def chart(request):
+    return render(request, "charts.html")
+
+def button(request):
+    return render(request, "buttons.html")
+
+def modal(request):
+    return render(request, "modals.html")
+
+def table(request):
+    return render(request, "tables.html")
+
+def homepage(request):
+    return HttpResponse("Hello, world. The dashboard is under construction. Try ../app/main/")
+
+def handler404(request):
+    response = render(request, 'index.html')
+    response.status_code = 404
+    return response
