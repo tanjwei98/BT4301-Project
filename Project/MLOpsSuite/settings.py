c77dad10daffe152b149dba2f49c5246656b7c6a
Sweta Mishra <m.swetab14@gmail.com>
2023-03-21 01:18:55 +0800

    Added

--- Project/MLOpsSuite/settings.py
+++ Project/MLOpsSuite/settings.py
@@ -73,13 +73,18 @@ WSGI_APPLICATION = "MLOpsSuite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+    
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BT4301', # CHANGE TO YOUR OWN
+        'USER': 'postgres',
+        'PASSWORD': 'sweta###456', # CHANGE TO YOUR OWN
+        'HOST': 'localhost',
+        'PORT': 5432, # CHANGE TO YOUR OWN
     }
 }
+
 
 
 # Password validation
