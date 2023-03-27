b27ed072110ae982ee160a43bb9f74c352cdf2ab
Eugene Lim <eugenelimel@yahoo.com>
2023-03-27 17:43:32 +0800

    Update new fields for tables in models.py

--- Project/core/models.py
+++ Project/core/models.py
@@ -7,34 +7,44 @@ utc=pytz.UTC
 
 class Dataset_List(models.Model):
     Dataset_name = models.CharField(max_length=255)
-    Dataset_id = models.CharField(max_length=255,primary_key=True)
+    Dataset_ID = models.CharField(max_length=255,primary_key=True)
     num_of_rows = models.IntegerField()
     num_of_features = models.IntegerField()
+    Target = models.CharField(max_length=255,null=False)
  
 class Users(models.Model):
     User_ID = models.CharField(max_length=255,primary_key=True)
     Name = models.CharField(max_length=255,null=False)
     Password = models.CharField(max_length=255,null=False)
     Role = models.CharField(max_length=255,null=False)
+    Supervisor = models.CharField(max_length=255,null=False)
  
 class Model_List(models.Model):
-    Model_ID = models.CharField(max_length=255,primary_key=True)
+    Model_ID = models.CharField(max_length=255)
     Model_name = models.CharField(max_length=255,null=False)
     Model_version = models.CharField(max_length=255,null=False)
     Language = models.CharField(max_length=255,null=False)
     User_ID = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
-    Dataset_id=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Dataset_ID=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Model_description=models.TextField()
+    Approve_Status=models.CharField(max_length=255,null=False)
+    Change_Comments=models.TextField()
+    Approve_Comments=models.TextField()
+    Date= models.DateTimeField(max_length=255,null=False)
+    Service_Health_Status=models.CharField(max_length=255,null=False)
+    Data_Drift_Status=models.CharField(max_length=255,null=False)
+    Accuracy_Status=models.CharField(max_length=255,null=False)
 
 class Service_Health(models.Model):
-    Response_time = models.CharField(max_length=255,null=False)
-    Execution_time = models.CharField(max_length=255,null=False)
+    Response_time = models.FloatField(max_length=255,null=False)
+    Execution_time = models.FloatField(max_length=255,null=False)
     Data_error_rate = models.FloatField(max_length=255,null=False)
     System_error_rate = models.FloatField(max_length=255,null=False)
-    Total_predictions = models.CharField(max_length=255,null=False)
+    Total_predictions = models.FloatField(max_length=255,null=False)
     Date = models.DateTimeField(null=False) 
     Location = models.CharField(max_length=255,null=False)
     Model_ID = models.ForeignKey(Model_List, on_delete=models.DO_NOTHING)
-    Dataset_id=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Dataset_ID=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
 
 class Model_Accuracy(models.Model):
     LogLoss = models.FloatField(max_length=255,null=False)
@@ -47,7 +57,7 @@ class Model_Accuracy(models.Model):
     Target = models.CharField(max_length=255,null=False)
     Predicted = models.CharField(max_length=255,null=False)
     Model_ID = models.ForeignKey(Model_List, on_delete=models.DO_NOTHING)
-    Dataset_id=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Dataset_ID=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
 
 class Feature_Distribution(models.Model):
     train_distribution= models.FloatField(max_length=255,null=False)
@@ -55,7 +65,7 @@ class Feature_Distribution(models.Model):
     Date= models.DateTimeField(max_length=255,null=False)
     Feature= models.CharField(max_length=255,null=False)
     Model_ID = models.ForeignKey(Model_List, on_delete=models.DO_NOTHING)
-    Dataset_id=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Dataset_ID=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
 
 class Model_drift(models.Model):
     Feature= models.CharField(max_length=255,null=False)
@@ -63,4 +73,4 @@ class Model_drift(models.Model):
     Importance= models.FloatField(max_length=255,null=False)
     Date= models.DateTimeField(max_length=255,null=False)
     Model_ID = models.ForeignKey(Model_List, on_delete=models.DO_NOTHING)
-    Dataset_id=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
+    Dataset_ID=models.ForeignKey(Dataset_List, on_delete=models.DO_NOTHING)
